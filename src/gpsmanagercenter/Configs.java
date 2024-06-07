b8529b31ca8628646ae68658220aaff1bea0655a
Nguyen Hai <xayenloc@gmail.com>
2024-06-07 18:18:18 +0700

    .

--- src/gpsmanagercenter/Configs.java
+++ src/gpsmanagercenter/Configs.java
@@ -1,7 +1,7 @@
 package gpsmanagercenter;
 
 public class Configs {
-    public static final String traccar_host="vngts.com";
+    public static final String traccar_host="ongps.vn";
     public static final int traccar_port=5055;
 
 
